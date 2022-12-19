e0f5578b832acde7d4de881e78de23f7b87a8bb9
linhnh <linhnh1@vmodev.com>
2022-12-19 13:36:03 +0700

    init project

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,28 +1,10 @@
 <template>
-  <form @submit.prevent="handleSubmit">
-    <div class="w-[650px] m-auto min-h-screen flex justify-center items-center">
-      <div
-        class="w-full login-form px-3 py-6 sm:!px-6 sm:!py-10 bg-[#000] bg-opacity-20 backdrop-blur-[10px] shadow-lg rounded-2"
-      >
-        <p class="pb-6 text-6 font-semibold">Login</p>
-        <TextInput
-          htmlFor="email"
-          label="Email"
-          v-model="email"
-          name="email"
-          class="mb-3"
-        />
-        <TextInput
-          htmlFor="password"
-          type="password"
-          label="Password"
-          v-model="password"
-          name="password"
-          class="mb-4"
-        />
-        <!-- <Button variant="primary" type="submit">Login</Button> -->
-        <button type="submit">link</button>
-      </div>
+  <div class="w-[650px] m-auto min-h-screen flex justify-center items-center">
+    <div class="w-full">
+      <p class="pb-6 text-6 font-semibold">Login</p>
+      <TextInput label="Email" class="mb-3" />
+      <TextInput type="password" label="Password" />
+      <Button variant="default">Login</Button>
     </div>
   </form>
 </template>
